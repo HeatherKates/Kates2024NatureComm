b6f24647390137517a58cd973d5892ff40627e31
HeatherKates <hkates@ufl.edu>
2024-02-02 16:02:12 -0500

    changed dir names, deleted files

--- EstimateRates_and_SelectBestModel/One_Rate/2_OneRate.R
+++
@@ -1,8 +0,0 @@
-library(corHMM)
-tipstate_matrix <- read.csv("../Nitfix.statedata.v7.csv",header=TRUE)
-tree <- read.tree("../Nitfix.tree.v7.tree")
-OneRates.NUM <- corHMM(tree,tipstate_matrix,1,rate.mat=NULL,
-                     node.states="none",fixed.nodes=FALSE,p=NULL,root.p=NULL,
-                     ip=CUSTOM,nstarts=0,n.cores=1,get.tip.states=FALSE,lewis.asc.bias=FALSE,
-                     lower.bound = 1e-9,upper.bound = 100)
-save.image("RData_100_Reps/OneRates.NUM.RData")
--- EstimateRates_and_SelectBestModel/Two_Precursor/0_Format_p_from_RData.R
+++
@@ -1,4 +0,0 @@
-load("RData_100_Reps/Species.3HRC.Precursor.12.RData")
-corHMM_fit=Species.3HRC.Precursor.12
-p = sapply(1:max(corHMM_fit$index.mat, na.rm = TRUE), function(x) na.omit(c(corHMM_fit$solution))[na.omit(c(corHMM_fit$index.mat) == x)][1])
-print(paste(as.character(p), collapse=","))
