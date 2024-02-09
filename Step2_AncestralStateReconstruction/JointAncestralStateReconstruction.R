39cb6b11b9efd183284198b0506424d5eb7aa1bf
HeatherKates <hkates@ufl.edu>
2024-02-09 15:51:53 -0500

    Added main fig anc recon R script

---
+++ Step2_AncestralStateReconstruction/JointAncestralStateReconstruction.R
@@ -0,0 +1,19 @@
+library(corHMM)
+Tips <- c("Sp1","Sp2","Sp3")
+State <- c("Nodulating","Non-nodulating","?")
+Data <- cbind(Tips,State)
+RateClassMat <- getRateCatMat(3)
+RateCat1 <- getStateMat4Dat(Data)$rate.mat
+RateCat2 <- getStateMat4Dat(Data)$rate.mat
+RateCat3 <- getStateMat4Dat(Data)$rate.mat
+#Make it so that rate cat 3 is the one that can't gain nodulation, remove 1 --> 2
+RateCat3[3] <- 0
+StateMats <- list(RateCat1, RateCat2,RateCat3)
+HR3.precursorish.FullMat <- getFullMat(StateMats, RateClassMat)
+tipstate_matrix <- read.csv("../Nitfix.statedata.v6.csv",header=TRUE)
+tree <- read.tree("../Nitfix.tree.v7.tree")
+v7.joint.bestML <- corHMM(tree,tipstate_matrix,3,rate.mat=HR3.precursorish.FullMat,
+                     node.states="joint",fixed.nodes=FALSE,p=c(0.000488147972793125,1e-09,5.60273948071007,2.6991240574648,1e-09,17.0584583593854,0.00118805251668781,1e-09,0.0670534220290817,5.30538518434259e-05,12.832403723037),root.p=NULL,
+                     ip=NULL,nstarts=0,n.cores=1,get.tip.states=FALSE,lewis.asc.bias=FALSE,
+                     lower.bound = 1e-9,upper.bound = 100)
+save.image("AncestralStateReconstruction.RData")
