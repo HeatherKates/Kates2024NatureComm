b41b419ca709c974338bf16a3a4f937d654e023b
HeatherKates <hkates@ufl.edu>
2024-02-09 16:02:07 -0500

    Updated input filepaths

--- Step2_AncestralStateReconstruction/JointAncestralStateReconstruction.AltBackbone.R
+++ Step2_AncestralStateReconstruction/JointAncestralStateReconstruction.AltBackbone.R
@@ -10,8 +10,8 @@ RateCat3 <- getStateMat4Dat(Data)$rate.mat
 RateCat3[3] <- 0
 StateMats <- list(RateCat1, RateCat2,RateCat3)
 HR3.precursorish.FullMat <- getFullMat(StateMats, RateClassMat)
-tipstate_matrix <- read.csv("Nitfix.statedata.v7.csv",header=TRUE)
-tree <- read.tree("DiffBB.v7.Nitfix.tree")
+tipstate_matrix <- read.csv("../InputDataFiles/TipStates.csv",header=TRUE)
+tree <- read.tree("../InputDataFiles/AlternativeTopology.Phylogeny.NoTaxonLabels.tree")
 edit.DiffBB.joint.bestML <- corHMM(tree,tipstate_matrix,3,rate.mat=HR3.precursorish.FullMat,
                      node.states="joint",fixed.nodes=FALSE,p=c(0.000804457357351217,1e-09,1e-09,0.0814636247178405,2.74257974809602,0.104767007545373,0.0236320709662464,1e-09,0.00730764831888938,1e-09,0.0234002743733769),root.p=NULL,
                      ip=NULL,nstarts=0,n.cores=1,get.tip.states=FALSE,lewis.asc.bias=FALSE,
